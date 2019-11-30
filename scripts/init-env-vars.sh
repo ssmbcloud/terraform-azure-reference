13010fc1b0a4eaf164051a29bb4a4b5f3d6a9148
ssmbcloud <ssmb.cloud@mail.com>
2019-11-30 01:45:50 -0600

    create storage account and update

--- scripts/init-env-vars.sh
+++ scripts/init-env-vars.sh
@@ -4,6 +4,6 @@ set -e
 
 export LOCATION=southcentralus
 export COMMON_RESOURCE_GROUP_NAME=terraform-ref-rg
-export TF_STATE_STORAGE_ACCOUNT_NAME=tfstaterefstore
+export TF_STATE_STORAGE_ACCOUNT_NAME=tfstatestore
 export TF_STATE_CONTAINER_NAME=tfstate-ref
 export KEYVAULT_NAME=terraform-ref-kv
